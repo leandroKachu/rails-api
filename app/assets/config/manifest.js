3db091da624af86898889935769e279ff02125aa
leandro.silva <leandro.silva@roit.com.br>
2022-11-10 17:48:44 -0300

    initial project api study

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,7 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link graphiql/rails/application.css
+//= link graphiql/rails/application.js
+//= link favicon/site.webmanifest
+//= link favicon/browserconfig.xml
+//= link_tree ../builds
